90b91e6753d96333c6d0fff64bdf8214a9ea9a5e
Sergey Rubtsov <rsg81@ya.ru>
2021-08-06 15:56:33 +0300

    Доделал модель, зарегил в админке

--- msd/ticket/models.py
+++ msd/ticket/models.py
@@ -6,21 +6,21 @@ User = get_user_model()
 
 class Ticket(models.Model):
     title = models.CharField(max_length=150, verbose_name='Тема')
-    content = models.TimeField(blank=True, verbose_name='Сообщение')
+    content = models.TextField(blank=True, verbose_name='Сообщение')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
-    image = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Изображение')
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Изображение', blank=True, null=True)
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name='Категория')
-    state = models.ForeignKey('State', on_delete=models.PROTECT, verbose_name='Состояние')
+    state = models.ForeignKey('State', on_delete=models.PROTECT, verbose_name='Статус')
     priority = models.ForeignKey('Priority', on_delete=models.PROTECT, verbose_name='Приоритет')
-    assigned = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Исполнитель')
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    assigned = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets_assigned', verbose_name='Исполнитель')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets_author', verbose_name='Автор')
 
     def __str__(self):
         return self.title
 
     class Meta:
-        verbose_name = 'Заявка'
+        verbose_name = 'Заявку'
         verbose_name_plural = 'Заявки'
         ordering = ['created_at']
 
